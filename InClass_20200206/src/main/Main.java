dc66baf05b3f5df505324cfd327a4c79cf783f12
kelly2rb <kelly2rb@MCD210-PC-15.uccc.uc.edu>
2020-02-06 11:49:43 -0500

    Declared and Instantiated an object for each class.

--- InClass_20200206/src/main/Main.java
+++ InClass_20200206/src/main/Main.java
@@ -1,11 +1,18 @@
 package main;
 
+import bank.Bank;
+import bank.BrickAndMortar;
+import bank.CafeBranch;
 import bank.Online;
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		// Declare and instantiate a Bank, an Online, a BrickandMorta, and a CafeBranch object
+		Bank myBank = new Bank();
+		Online mySite = new Online();
+		BrickAndMortar myStore = new BrickAndMortar();
+		CafeBranch myCafe = new CafeBranch();
 	}
 
 }
